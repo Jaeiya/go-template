e65e2e6e7bc01f9ebe8238e99b3886f5f1a07b9e
Jaeiya <exanix@gmail.com>
2026-08-08 16:15:15 -0700

    refactor: rename `lib` to `internal`
    This follows standard Go practice

--- cmd/name/main.go
+++ cmd/name/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/jaeiya/go-template/lib"
+	"github.com/jaeiya/go-template/internal"
 )
 
 func main() {
 	fmt.Println("hello world")
-	lib.SomeNecessaryFunc()
+	internal.SomeNecessaryFunc()
 }
